fcaff8748ed9b7e66e4f8ff177af809081dd356b
Harsh Shandilya <msfjarvis@gmail.com>
2017-07-21 17:41:50 +0530

    KronicBot: Add ability to run shell commands from telegram
    Kanged mostly from https://github.com/bvanrijn/sudobot/blob/master/bot.py

--- build.py
+++ build.py
@@ -8,9 +8,9 @@ import subprocess
 import time
 
 import sys
-from telegram import ChatAction
-from telegram.ext import CommandHandler
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
+from telegram import InlineQueryResultArticle, ChatAction, InputTextMessageContent
+from uuid import uuid4
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -69,6 +69,49 @@ def restart(bot, update):
     else:
         sendNotAuthorizedMessage(bot, update)
 
+def execute(bot, update, direct=True):
+
+    try:
+        user_id = update.message.from_user.id
+        command = update.message.text
+        inline = False
+    except AttributeError:
+        # Using inline
+        user_id = update.inline_query.from_user.id
+        command = update.inline_query.query
+        inline = True
+
+    if user_id == int(sudo_users):
+        if not inline:
+            bot.sendChatAction(chat_id=update.message.chat_id,
+                               action=ChatAction.TYPING)
+        output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output = output.stdout.read().decode('utf-8')
+        output = '`{0}`'.format(output)
+
+        if not inline:
+            bot.sendMessage(chat_id=update.message.chat_id,
+                        text=output, parse_mode="Markdown")
+            return False
+
+        if inline:
+            return output
+
+
+def inlinequery(bot, update):
+    query = update.inline_query.query
+    o = execute(query, update, direct=False)
+    results = list()
+
+    results.append(InlineQueryResultArticle(id=uuid4(),
+                                            title=query,
+                                            description=o,
+                                            input_message_content=InputTextMessageContent(
+                                                '*{0}*\n\n{1}'.format(query, o),
+                                                parse_mode="Markdown")))
+
+    bot.answerInlineQuery(update.inline_query.id, results=results, cache_time=10)
+
 def isAuthorized(update):
     return update.message.from_user.id == int(sudo_users)
 
@@ -82,10 +125,13 @@ def sendNotAuthorizedMessage(bot, update):
 build_handler = CommandHandler('build', build)
 upload_handler = CommandHandler('upload', upload)
 restart_handler = CommandHandler('restart', restart)
+execute_handler = MessageHandler([Filters.text], execute)
 
 dispatcher.add_handler(build_handler)
 dispatcher.add_handler(upload_handler)
 dispatcher.add_handler(restart_handler)
+dispatcher.add_handler(execute_handler)
+dispatcher.add_handler(InlineQueryHandler(inlinequery))
 
 updater.start_polling()
 updater.idle()
